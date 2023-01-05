5b50a9c2e2b5e44db912822fd1dcae97edb28d2f
AndyOlsen1295 <112365233+AndyOlsen1295@users.noreply.github.com>
2023-01-04 23:26:39 -0700

    added array of questions to Index.js

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [console.log(
+  "what is the title of your web-page?",
+  "what license would you like to add? (leave blank if none)",
+  "What is the purpose of this project? What problem does it solve? What was the motivation behind it?",  
+  "What will the expected user experience be?",
+  "Would you like to ask for contributions?",
+  "Is your project complete? (if so paste URL here)",
+)];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
